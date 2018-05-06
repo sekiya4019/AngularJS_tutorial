e3b76a8f15570917db1a66ba1a2b1e31c391d490
sekiya4019 <sekiya4019@gmail.com>
2018-05-06 16:54:57 +0900

    Before code copy

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
+
+// import { Observable } from 'rxjs/Observable';
+// import { of } from 'rxjs/observable/of';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +13,22 @@ import { HEROES } from './mock-heroes';
 
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
+  // getHeroes(): Observable<Hero[]> {
+  //   return of(HEROES);
+  // }
+
+  getMessages(): void {
+    this.messages = this.MessageService;
+  }
+  // getHeroes(): Observable<Hero[]> {
+  //   // Todo: send the message _after_ fetching the heroes
+  //   this.messageService.add('HeroService: fetched heroes');
+  //   return of(HEROES);
+  // }
 
 }
